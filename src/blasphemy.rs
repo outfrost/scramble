6a3c7636831da2632612741b409450fd9d02ebe5
outfrost <kotlet.bahn@gmail.com>
2021-09-05 01:16:02 +0200

    Draw input box

--- src/blasphemy.rs
+++ src/blasphemy.rs
@@ -7,6 +7,11 @@ enum Input {
 	Quit,
 }
 
+struct Vector {
+	pub x: i32,
+	pub y: i32,
+}
+
 pub fn run() {
 	init();
 	loop {
@@ -42,6 +47,34 @@ fn input() -> Input {
 
 fn draw() {
 	erase();
+
+	let mut term_size = Vector { x: 0, y: 0 };
+	getmaxyx(stdscr(), &mut term_size.y, &mut term_size.x);
+
+	draw_input_box(term_size);
+
     addstr("Hello, world!");
     refresh();
+}
+
+fn draw_input_box(term_size: Vector) {
+	const BOX: [&str; 4] = [
+		"        TYPE A WORD         ",
+		"|==========================|",
+		"|                          |",
+		"|==========================|",
+	];
+
+	let mut line_pos = Vector { x: 0, y: 0 };
+	getyx(stdscr(), &mut line_pos.y, &mut line_pos.x);
+
+	line_pos.x = (term_size.x - BOX[0].len() as i32) / 2;
+	line_pos.y += 2;
+
+	for s in BOX {
+		mvaddstr(line_pos.y, line_pos.x, s);
+		line_pos.y += 1;
+	}
+
+	addstr("\n");
 }
