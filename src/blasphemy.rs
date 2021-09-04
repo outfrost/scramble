ae44e2f70f62fe5e946805ce595e23cb5980eb3e
outfrost <kotlet.bahn@gmail.com>
2021-09-04 23:41:52 +0200

    Create event loop?

--- src/blasphemy.rs
+++ src/blasphemy.rs
@@ -1,9 +1,47 @@
 use ncurses::*;
 
+const INPUT_TIMEOUT: i32 = 16; // milliseconds
+
+enum Input {
+	None,
+	Quit,
+}
+
 pub fn run() {
-    initscr();
+	init();
+	loop {
+		match input() {
+			Input::Quit => break,
+			_ => (),
+		}
+		draw();
+	}
+    terminate();
+}
+
+fn init() {
+	initscr();
+	keypad(stdscr(), true);
+	cbreak();
+	timeout(INPUT_TIMEOUT);
+}
+
+fn terminate() {
+	endwin();
+}
+
+fn input() -> Input {
+    let key = getch();
+    if key == KEY_F4 {
+    	Input::Quit
+    }
+    else {
+    	Input::None
+    }
+}
+
+fn draw() {
+	erase();
     addstr("Hello, world!");
     refresh();
-    getch();
-    endwin();
 }
